11f04ea895a5cc4ad49f0651d0b603320c6e0cf1
Carlos Renato Lombardero <carlos.lombardero@isobar.com.br>
2018-02-05 16:05:49 -0200

    Include tcode command to touch a file and open in code.

--- rena.plugin.zsh
+++ rena.plugin.zsh
@@ -3,3 +3,12 @@ alias extip="curl -s http://checkip.dyndns.org/ | sed 's/[a-zA-Z<>/ :]//g'"
 alias cd..="cd .."
 alias reload-zsh="source ~/.zshrc"
 alias edit-rena-plugin="code $ZSH_CUSTOM/plugins/rena/"
+
+tcode() {
+    if [[ "$#" != 0 ]]; then
+        touch "${@}" && code "${@}"
+    else
+        echo "Error: must specify at least one filename:"
+        echo "    tcode index.js"
+    fi
+}
