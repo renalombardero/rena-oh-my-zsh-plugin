c586f0501fba2661742bfdcc9a9b300da721038d
Renato Lombardero <carlos.lombardero@isobar.com.br>
2019-11-05 11:33:09 -0300

    Fix is-online and just-online functions. Add file counter

--- rena.plugin.zsh
+++ rena.plugin.zsh
@@ -1,15 +1,23 @@
 alias intip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
 alias extip="curl -s http://checkip.dyndns.org/ | sed 's/[a-zA-Z<>/ :]//g'"
 alias cd..="cd .."
+alias cfiles="find . -maxdepth 1 -exec echo \; | wc -l"
 alias reload-zsh="source ~/.zshrc"
 alias edit-rena-plugin="code $ZSH_CUSTOM/plugins/rena/"
 alias fix-audio="sudo killall coreaudiod"
 
-tcode() {
-    if [[ "$#" != 0 ]]; then
-        touch "${@}" && code "${@}"
+is-online() {
+    if curl --output /dev/null --silent --head --fail "${@}"; then
+    echo "✅ ${@}"
     else
-        echo "Error: must specify at least one filename:"
-        echo "    tcode index.js"
+    echo "❌ ${@}"
+    fi
+}
+
+just-offline() {
+    if curl --output /dev/null --silent --head --fail "${@}"; then
+    # echo "✅ ${@}"
+    else
+    echo "❌ ${@}"
     fi
 }
